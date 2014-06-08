8141258f5740fdc628d7838405e99ac64c19964f
Marco Yuen <marcoy@gmail.com>
2014-06-08 19:42:01 -0400

    Initial commit

---
+++ src/Chapter13/Monads.hs
@@ -0,0 +1,19 @@
+module Chapter13.Monads where
+
+
+import Control.Monad.State
+
+
+type Stack = [Int]
+
+pop :: Stack -> (Int,Stack)
+pop (x:xs) = (x,xs)
+
+push :: Int -> Stack -> ((),Stack)
+push a xs = ((),a:xs)
+
+stackManip :: Stack -> (Int,Stack)
+stackManip stack = let
+    ((),newStack1) = push 3 stack
+    (a ,newStack2) = pop newStack1
+    in pop newStack2
