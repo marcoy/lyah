7c3a4b1dcb3a5875cf1e2267e1c976ab514e13c5
Marco Yuen <marcoy@gmail.com>
2014-06-21 18:55:50 -0400

    Add ErrorT, and step by step transformers doc source.

--- src/Transformers/StepByStep.hs
+++ src/Transformers/StepByStep.hs
@@ -60,3 +60,46 @@ eval1 env (App e1 e2)  = do val1 <- eval1 env e1
                             case val1 of
                                 FunVal env' n body ->
                                     eval1 (Map.insert n val2 env') body
+
+-- λ> runEval1 (eval1 Map.empty exampleExp)
+
+-- 2.2 Adding error Handling
+type Eval2 a = ErrorT String Identity a
+
+runEval2 :: Eval2 a -> Either String a
+runEval2 ev = runIdentity (runErrorT ev)
+
+eval2a :: Env -> Exp -> Eval2 Value
+eval2a env (Lit i)      = return $ IntVal i
+eval2a env (Var n)      = maybe (fail ("undefined variable: " ++ n)) return $ Map.lookup n env
+eval2a env (Plus e1 e2) = do IntVal i1 <- eval2a env e1
+                             IntVal i2 <- eval2a env e2
+                             return $ IntVal (i1 + i2)
+eval2a env (Abs n e)    = return $ FunVal env n e
+eval2a env (App e1 e2)  = do val1 <- eval2a env e1
+                             val2 <- eval2a env e2
+                             case val1 of
+                                 FunVal env' n body ->
+                                    eval2a (Map.insert n val2 env') body
+
+-- λ> runEval1 (eval1 Map.empty exampleExp)
+
+eval2b :: Env -> Exp -> Eval2 Value
+eval2b env (Lit i)      = return $ IntVal i
+eval2b env (Var n)      = case Map.lookup n env of
+                              Nothing  -> throwError ("unbound variable: " ++ n)
+                              Just val -> return val
+eval2b env (Plus e1 e2) = do e1' <- eval2b env e1
+                             e2' <- eval2b env e2
+                             case (e1',e2') of
+                                 (IntVal i1, IntVal i2) -> return $ IntVal (i1 + i2)
+                                 _ -> throwError "type error in addition"
+eval2b env (Abs n e)    = return $ FunVal env n e
+eval2b env (App e1 e2)  = do val1 <- eval2b env e1
+                             val2 <- eval2b env e2
+                             case val1 of
+                                 FunVal env' n body ->
+                                    eval2b (Map.insert n val2 env') body
+                                 _ -> throwError "type error in application"
+
+-- λ> runEval2 (eval2a Map:empty (Plus (Lit 1) (Abs "x" (Var "x"))))
