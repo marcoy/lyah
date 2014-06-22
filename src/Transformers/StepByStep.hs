7debebe025f6b142648fd70f2d6977112eaefa56
Marco Yuen <marcoy@gmail.com>
2014-06-22 12:05:03 -0400

    Add StateT

--- src/Transformers/StepByStep.hs
+++ src/Transformers/StepByStep.hs
@@ -8,8 +8,12 @@ import Control.Monad.Reader
 import Control.Monad.State
 import Control.Monad.Writer
 import Data.Maybe
+import Debug.Trace
 import qualified Data.Map as Map
 
+
+traceShow' :: (Show a) => a -> a
+traceShow' a = traceShow a a
 
 type Name = String
 data Exp = Lit Integer
@@ -109,7 +113,7 @@ eval2 env (App e1 e2)  = do val1 <- eval2 env e1
 -- λ> runEval2 (eval2 Map.empty (Plus (Lit 1) (Abs "x" (Var "x"))))
 
 --
--- Hiding the environment
+-- 2.3 Hiding the environment
 --
 -- definition: ReaderT  r             m             a
 type Eval3 a = ReaderT Env (ErrorT String Identity) a
@@ -118,23 +122,62 @@ runEval3 :: Env -> Eval3 a -> Either String a
 runEval3 env ev = runIdentity (runErrorT (runReaderT ev env))
 
 eval3 :: Exp -> Eval3 Value
-eval3 (Lit i) = return $ IntVal i
-eval3 (Var n) = do env <- ask
-                   case Map.lookup n env of
-                       Nothing  -> throwError ("unbound variable: " ++ n)
-                       Just val -> return val
+eval3 (Lit i)      = return $ IntVal i
+eval3 (Var n)      = do env <- ask
+                        case Map.lookup n env of
+                            Nothing  -> throwError ("unbound variable: " ++ n)
+                            Just val -> return val
 eval3 (Plus e1 e2) = do e1' <- eval3 e1
                         e2' <- eval3 e2
                         case (e1',e2') of
                             (IntVal i1, IntVal i2) -> return $ IntVal (i1 + i2)
                             _                      -> throwError "type error in addition"
-eval3 (Abs n e) = do env <- ask
-                     return $ FunVal env n e
-eval3 (App e1 e2) = do val1 <- eval3 e1
-                       val2 <- eval3 e2
-                       case val1 of
-                           FunVal env' n body ->
-                               local (const (Map.insert n val2 env')) (eval3 body)
-                           _ -> throwError "type error in application"
+eval3 (Abs n e)    = do env <- ask
+                        return $ FunVal env n e
+eval3 (App e1 e2)  = do val1 <- eval3 e1
+                        val2 <- eval3 e2
+                        case val1 of
+                            FunVal env' n body ->
+                                local (const (Map.insert n val2 env')) (eval3 body)
+                            _ -> throwError "type error in application"
 
 -- λ> runEval3 Map.empty (eval3 exampleExp)
+
+--
+-- 2.4 Adding state
+--
+type Eval4 a = ReaderT Env (ErrorT String (StateT Integer Identity)) a
+
+runEval4 :: Env -> Integer -> Eval4 a -> (Either String a, Integer)
+runEval4 env st ev = runIdentity (runStateT (runErrorT (runReaderT ev env)) st)
+
+tick :: (Num s, MonadState s m) => m ()
+tick = do st <- get
+          put (st + 1)
+
+eval4 :: Exp -> Eval4 Value
+eval4 (Lit i)      = do tick
+                        return $ IntVal i
+eval4 (Var n)      = do tick
+                        env <- ask
+                        case Map.lookup n env of
+                            Nothing  -> throwError ("unbound variable: " ++ n)
+                            Just val -> return val
+eval4 (Plus e1 e2) = do tick
+                        e1' <- eval4 e1
+                        e2' <- eval4 e2
+                        case (e1',e2') of
+                            (IntVal i1, IntVal i2) -> return $ IntVal (i1 + i2)
+                            _                      -> throwError "type error in addition"
+eval4 (Abs n e)    = do tick
+                        env <- ask
+                        return $ FunVal env n e
+eval4 (App e1 e2)  = do tick
+                        val1 <- eval4 e1
+                        val2 <- eval4 e2
+                        case val1 of
+                            FunVal env' n body ->
+                                local (const (Map.insert n val2 env')) (eval4 body)
+                            _ -> throwError "type error in application"
+
+-- λ> runEval4 Map.empty 0 (eval4 exampleExp)
